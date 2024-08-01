3fe44dae5bf9768e339adc887d1fd854a2ac251f
Hans <hans.r.1999@gmail.com>
2024-08-01 23:40:14 +0200

    test: test http-requests via intellij http-client and run in github action

---
+++ http-test/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set +e
+while
+(exec 6<>/dev/tcp/localhost/8080) 2>/dev/null
+[ $? == 1 ]
+do echo Waiting for Go Drink; sleep 1; done
+set -e
+
+docker run --network="host" --rm -v $PWD:/workdir jetbrains/intellij-http-client -e dev -v http-test/http-client.env.json -r http-test-reports  http-test/*.http
