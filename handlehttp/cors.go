55240bc93ec2c11def1215774935d2983cf5f18f
Hans <hans.r.1999@gmail.com>
2024-09-12 20:30:27 +0200

    refactor: extract setCorsHeaders to function

--- handlehttp/cors.go
+++ handlehttp/cors.go
@@ -13,13 +13,16 @@ func AddCorsHeader(config CorsConfig, next GetResponseMapper) GetResponseMapper 
 	return func(r *http.Request) *ResponseMapper {
 		mapper := next(r)
 		var newMapper ResponseMapper = func(w http.ResponseWriter, status int, data any) {
-			if config.AddCorsHeader {
-				w.Header().Set("Access-Control-Allow-Origin", config.CorsWhitelist)
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
-			}
-
+			setCorsHeaders(w, config)
 			(*mapper)(w, status, data)
 		}
 		return &newMapper
+	}
+}
+
+func setCorsHeaders(w http.ResponseWriter, config CorsConfig) {
+	if config.AddCorsHeader {
+		w.Header().Set("Access-Control-Allow-Origin", config.CorsWhitelist)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 	}
 }
