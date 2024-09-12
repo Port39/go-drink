61e3e8b9cd5152e02266a30571bae88759ce85f7
Hans <hans.r.1999@gmail.com>
2024-09-12 20:36:20 +0200

    feat: allow separate base-template and content in html

--- handlehttp/html-mapper.go
+++ handlehttp/html-mapper.go
@@ -8,16 +8,25 @@ import (
 )
 
 func HtmlMapper(tplFS fs.FS, tplPaths ...string) ResponseMapper {
-	tpl := template.Must(template.ParseFS(tplFS, tplPaths...))
+	templates := append(tplPaths, "base-templates/page-layout.gohtml")
+	tpl := template.Must(template.ParseFS(tplFS, templates...))
 
 	return func(w http.ResponseWriter, status int, data any) {
 		w.Header().Set("content-type", Html.String())
 		w.WriteHeader(status)
-		err := tpl.Execute(w, tpl)
+		err := executeWholePageTemplate(w, tpl, data)
 
 		if err != nil {
 			log.Println("Error while writing html response:", err)
 			return
 		}
 	}
+}
+
+func executeWholePageTemplate(w http.ResponseWriter, t *template.Template, data interface{}) error {
+	return t.ExecuteTemplate(w, "base", data)
+}
+
+func executeFragmentTemplate(w http.ResponseWriter, t *template.Template, data interface{}) error {
+	return t.ExecuteTemplate(w, "fragment", data)
 }
